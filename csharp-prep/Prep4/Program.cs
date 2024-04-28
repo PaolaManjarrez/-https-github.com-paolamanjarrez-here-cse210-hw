85da8e0012a355e3d286b1a4cc20ce8d7d917799
PaolaManjarrez <paomanjtav@gmail.com>
2024-04-27 23:21:59 -0500

    prep 4

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,48 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        List<int> numbers = new List<int>();
+        
+        int user;
+        do
+        {
+            Console.Write("Enter a number: ");
+            string number = Console.ReadLine();
+            
+            if (!int.TryParse(number, out user))
+            {
+                Console.WriteLine("Invalid number. Please enter a valid integer.");
+                continue;
+            }
+            
+            if (user != 0)
+            {
+                numbers.Add(user);
+            }
+        } while (user != 0);
+
+        if (numbers.Count > 0)
+        {
+            int sum = 0;
+            foreach (int number in numbers)
+            {
+                sum += number;
+            }
+            Console.WriteLine($"The sum is: {sum}");
+
+            float average = (float)sum / numbers.Count;
+            Console.WriteLine($"The average is: {average}");
+
+            int max = numbers.Max();
+            Console.WriteLine($"The max is: {max}");
+        }
+        else
+        {
+            Console.WriteLine("No numbers were entered.");
+        }
     }
 }
